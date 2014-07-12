7e8d265a69739518b0ef02f9c614882edac51936
Nick Worth <nworth@raprec.com>
2014-07-11 18:36:50 -0700

    update homepage and nav layout and styles

---
+++ inc/sidebars.php
@@ -0,0 +1,59 @@
+<?php 
+/**
+ * Div Sidebars
+ * Sidebars are registered here, any additional sidebars can be added. 
+ * Also consider using WooSidebars (https://wordpress.org/plugins/woosidebars/)
+ *
+ * @author    Div Truth
+ * @category  Framework/Features
+ * @license   GPL
+ */
+
+/************* ACTIVE SIDEBARS ********************/
+# Include Div Sidebars
+include(DIV_INC_DIR.'sidebars.php');
+
+# Activate Div Sidebars (WARNING: ^framework sidebars must be included)
+add_action( 'widgets_init', 'div_child_register_sidebars' );
+
+function div_child_register_sidebars() {
+
+    /* HEADER SIDERBARS */
+    register_nav_social_icons(FALSE);
+    register_header_right(FALSE);
+    
+    /* STANDARD SIDEBARS */
+    register_primary_sidebar(TRUE);
+    register_secondary_sidebar(FALSE);
+
+    /* FOOTER SIDEBARS */
+    register_footer_1(FALSE);
+    register_footer_2(FALSE);
+    register_footer_3(FALSE);
+    register_footer_4(FALSE);
+
+    /* CUSTOM TEMPLATES */
+    
+    //register_sidebar(array(
+        //'id' => 'features',
+        //'name' => __('Features', 'div_theme'),
+        // 'description' => __('The features sidebar.', 'div_theme'),
+        //'before_widget' => '<div id="%1$s" class="widget %2$s">',
+        //'after_widget' => '</div>',
+        //'before_title' => '<h4 class="widgettitle">',
+        //'after_title' => '</h4>',
+    //));
+
+    // register_sidebar(array(
+    //     'id' => 'cta',
+    //     'name' => __('Call to Action', 'div_theme'),
+    //     'description' => __('The Call to Action sidebar.', 'div_theme'),
+    //     'before_widget' => '<div id="%1$s" class="widget %2$s">',
+    //     'after_widget' => '</div>',
+    //     'before_title' => '',
+    //     'after_title' => '',
+    // ));
+      
+} 
+
+?>
