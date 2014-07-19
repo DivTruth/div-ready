41621975f40af35ec36a5d1d9bfcc7d06471ccad
Nick Worth <nworth@raprec.com>
2014-07-18 17:31:21 -0700

    restructure appearance/ model

--- functions.php
+++ functions.php
@@ -15,10 +15,10 @@
  */
 add_action('after_setup_theme', 'theme_action_adjustments', 99);
 function theme_action_adjustments(){
-	DF::add_action('div_post_content',array('div_post_info'=>12) );
+	DF::add_action('div_post_content', array('div_post_info'=>12) );
+
 }
 
-
-
-# Remove ACF Menu
-// remove_menu_page('edit.php?post_type=acf'); 
+add_filter( 'post_info_date', function($s){
+	return '<i class="fa fa-calendar">test</i>'.$s;
+});
