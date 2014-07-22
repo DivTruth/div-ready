43ff903b4092368e04fe65e8f50d5a11916287a0
Nick Worth <nworth@raprec.com>
2014-07-22 14:16:52 -0700

    modify styles for new framework menus

--- functions.php
+++ functions.php
@@ -15,10 +15,18 @@
  */
 add_action('after_setup_theme', 'theme_action_adjustments', 99);
 function theme_action_adjustments(){
-	DF::add_action('div_post_content', array('div_post_info'=>12) );
+	DF::add_action('df_post_content', array('df_post_info'=>12) );
 
 }
 
 add_filter( 'post_info_date', function($s){
 	return '<i class="fa fa-calendar">test</i>'.$s;
 });
+
+
+// $my_theme = wp_get_theme( 'twentyten' );
+// if ( $my_theme->exists() )
+// 	echo $my_theme;
+
+# Remove ACF Menu
+// remove_menu_page('edit.php?post_type=acf'); 
