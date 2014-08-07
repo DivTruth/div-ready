6482ea7aef7e0e6a6a64edc3f09257d9e3222bca
Nick Worth <nworth@raprec.com>
2014-08-06 20:49:11 -0700

    update nav styles for df_nav_logo

--- functions.php
+++ functions.php
@@ -2,27 +2,27 @@
 /**
  * @package 	Div-Framework
  * @subpackage 	Div-Ready
- * @author  	Div Truth
+ * @author  	Div Blend Team
  * @license 	GPL
  *
  */
 
 /** Actions ****************************************************************/
 
-	/**
-	 * Reorder the post_content call
-	 */
 	add_action('after_setup_theme', 'theme_action_adjustments', 99);
 	function theme_action_adjustments(){
-		DF::add_action('df_post_content', array('df_post_info'=>12) );
+		/**
+		 * Reorder the post_content call
+		 */
+		// DF::add_action('df_post_content', array('df_post_info'=>15) );
 	}
-
 
 /** Filters ****************************************************************/
 
 	/**
-	 * The body is hidden until the CSS is loaded so that nothing appears until it is styled,
-	 * therefore the CSS must reset this to be displayed.
+	 * CSS DELAY (Built for faster load times)
+	 * Sets the body to hidden until the CSS is loaded (from footer) so that nothing appears until it is styled,
+	 * then the CSS must reset this to be displayed. 
 	 */
 	add_filter( 'body_styles', function($s){
 		return 'opacity:0;filter: alpha(opacity=0); display:none;'.$s;
